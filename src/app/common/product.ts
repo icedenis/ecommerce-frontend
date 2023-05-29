7aa1c4659f98d2d8d003f64867480f548df7aa00
Rahim Denis (ext) <rahide@akros.ch>
2023-05-29 15:39:33 +0300

    New Project

---
+++ src/app/common/product.ts
@@ -0,0 +1,17 @@
+export class Product {
+
+    
+    constructor(public sku:string,
+        public name:string,
+        public description:string,
+        public unitPrice:number,
+        public imgUrl: string,
+        public active: boolean,
+        public unitsInStock: number,
+        public dateCreated: Date,
+        public lastUpdate: Date
+        )
+    {
+        
+    }
+}
